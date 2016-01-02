da93b902550a238aa992c6337e1c9ef5ca84f8ca
timduente <tim.duente@t-online.de>
2016-01-02 11:30:42 +0100

    it runs.

--- src/main/Viewport3d.java
+++ src/main/Viewport3d.java
@@ -12,75 +12,93 @@ import javax.media.j3d.*;
 /**
  * Three dimensional viewport for viewing the dicom images + segmentations.
  * 
- * @author  Karl-Ingo Friese
+ * @author Karl-Ingo Friese
  */
 @SuppressWarnings("serial")
-public class Viewport3d extends Viewport implements Observer  {
-	
-	double rot = Math.PI /4.0;
+public class Viewport3d extends Viewport implements Observer {
+
+	double rot = Math.PI / 4.0;
+
 	/**
 	 * Private class, implementing the GUI element for displaying the 3d data.
 	 */
 	public class Panel3d extends Canvas3D {
 		public SimpleUniverse _simple_u;
 		public BranchGroup _scene;
-		
-		
-		public Panel3d( GraphicsConfiguration config ){	
-			super( config );
-			setMinimumSize(new Dimension(DEF_WIDTH,DEF_HEIGHT));
-			setMaximumSize(new Dimension(DEF_WIDTH,DEF_HEIGHT));
-			setPreferredSize(new Dimension(DEF_WIDTH,DEF_HEIGHT));
+
+		public Panel3d(GraphicsConfiguration config) {
+			super(config);
+			setMinimumSize(new Dimension(DEF_WIDTH, DEF_HEIGHT));
+			setMaximumSize(new Dimension(DEF_WIDTH, DEF_HEIGHT));
+			setPreferredSize(new Dimension(DEF_WIDTH, DEF_HEIGHT));
 			setBackground(Color.black);
 
 			_simple_u = new SimpleUniverse(this);
-		    _simple_u.getViewingPlatform().setNominalViewingTransform();
-		    _scene = null;
-		    createScene();
+			_simple_u.getViewingPlatform().setNominalViewingTransform();
+			_scene = null;
+			createScene();
+
+			new Thread(new Runnable() {
+				public void run() {
+					while (true) {
+						rot = rot + 0.02;
+						update_view();
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+					}
+				}
+
+			}).start();
 		}
-	 
+
 		public void createScene() {
 			if (_scene != null) {
-				_scene.detach( );
+				_scene.detach();
 			}
 			_scene = new BranchGroup();
-			_scene.setCapability( BranchGroup.ALLOW_DETACH );
-			
+			_scene.setCapability(BranchGroup.ALLOW_DETACH);
+
 			rot = rot + 0.1;
-			
+
 			Transform3D rotate = new Transform3D();
 			rotate.rotY(rot);
-			Transform3D rot2 =new Transform3D();
+			Transform3D rot2 = new Transform3D();
 			rot2.rotX(rot);
 			TransformGroup rotate_group = new TransformGroup(rotate);
 			TransformGroup rotate_group2 = new TransformGroup(rot2);
 			rotate_group.addChild(rotate_group2);
 			_scene.addChild(rotate_group);
 			rotate_group2.addChild(new ColorCube(0.4));
-			
+
 			_scene.compile();
 			_simple_u.addBranchGraph(_scene);
-			
-			
-		}
-			
-	}		
 
+		}
+
+	}
 
 	private Panel3d _panel3d;
 
 	/**
 	 * Constructor, with a reference to the global image stack as argument.
-	 * @param slices a reference to the global image stack
+	 * 
+	 * @param slices
+	 *            a reference to the global image stack
 	 */
 	public Viewport3d() {
 		super();
-		
-		this.setPreferredSize(new Dimension(DEF_WIDTH,DEF_HEIGHT));
-		this.setLayout( new BorderLayout() );
-		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
-		_panel3d = new Panel3d( config );		
-        this.add(_panel3d, BorderLayout.CENTER );        
+
+		this.setPreferredSize(new Dimension(DEF_WIDTH, DEF_HEIGHT));
+		this.setLayout(new BorderLayout());
+		GraphicsConfiguration config = SimpleUniverse
+				.getPreferredConfiguration();
+		_panel3d = new Panel3d(config);
+		this.add(_panel3d, BorderLayout.CENTER);
 	}
 
 	/**
@@ -89,28 +107,27 @@ public class Viewport3d extends Viewport implements Observer  {
 	public void update_view() {
 		_panel3d.createScene();
 	}
-	
+
 	/**
-	 * Implements the observer function update. Updates can be triggered by the global
-	 * image stack.
+	 * Implements the observer function update. Updates can be triggered by the
+	 * global image stack.
 	 */
-	public void update(final Observable o, final Object obj ) {
+	public void update(final Observable o, final Object obj) {
 		if (!EventQueue.isDispatchThread()) {
-			// all swing thingies must be done in the AWT-EventQueue 
+			// all swing thingies must be done in the AWT-EventQueue
 			EventQueue.invokeLater(new Runnable() {
 				public void run() {
-					update(o,obj);
+					update(o, obj);
 				}
 			});
 			return;
 		}
 
 		// boolean update_needed = false;
-		Message m = (Message)obj;
-		update_view();
-		
+		Message m = (Message) obj;
+
 		if (m._type == Message.M_SEG_CHANGED) {
-			String seg_name = ((Segment)(m._obj)).getName();
+			String seg_name = ((Segment) (m._obj)).getName();
 			boolean update_needed = _map_name_to_seg.containsKey(seg_name);
 			if (update_needed) {
 				update_view();
